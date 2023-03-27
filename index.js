5a4c2e73912df1e59c55cccc673c8bd27d953b07
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-26 19:31:10 -0700

    Completes functions cont.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = `roller-skate`, message) {
+   return message = `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = `go to the office`, message) {
+   return message = `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair) {
+    return function(description = `special`) {
+        return `You are ${flair}${description}${flair}!`;
+    }
+}
